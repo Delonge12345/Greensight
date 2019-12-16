6a538ad997a9f8ba4685676d521e12ca6feb3dd2
Delonge12345 <lobkov1997@gmail.com>
2019-12-16 08:05:17 +0300

    Updates

---
+++ precache-manifest.4c6afcdda623db0783f8e4e7808ea511.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fddcc279e4628f1836043efa4125bd8f",
+    "url": "/Greensight/index.html"
+  },
+  {
+    "revision": "f62b35cb3c0e26cac6ce",
+    "url": "/Greensight/static/css/5.7cbf0f1e.chunk.css"
+  },
+  {
+    "revision": "7cb8fe8cf8b42032299b",
+    "url": "/Greensight/static/css/6.997a27dc.chunk.css"
+  },
+  {
+    "revision": "fc3e4a3ad5e23f93eecd",
+    "url": "/Greensight/static/css/main.0767fd5d.chunk.css"
+  },
+  {
+    "revision": "f94976b13c6a954e3aa6",
+    "url": "/Greensight/static/js/0.57bb1473.chunk.js"
+  },
+  {
+    "revision": "c5884aef247690d12822",
+    "url": "/Greensight/static/js/3.061154bb.chunk.js"
+  },
+  {
+    "revision": "fc1f0c050587833cbe97",
+    "url": "/Greensight/static/js/4.c1191aab.chunk.js"
+  },
+  {
+    "revision": "f62b35cb3c0e26cac6ce",
+    "url": "/Greensight/static/js/5.6a99ceec.chunk.js"
+  },
+  {
+    "revision": "7cb8fe8cf8b42032299b",
+    "url": "/Greensight/static/js/6.00c95df2.chunk.js"
+  },
+  {
+    "revision": "6481992e839f50a0f104",
+    "url": "/Greensight/static/js/7.e415536c.chunk.js"
+  },
+  {
+    "revision": "fc3e4a3ad5e23f93eecd",
+    "url": "/Greensight/static/js/main.4ee598ec.chunk.js"
+  },
+  {
+    "revision": "23f7a6c9b7e4bc4b5c67",
+    "url": "/Greensight/static/js/runtime-main.3286fc38.js"
+  }
+]);
